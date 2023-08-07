065febea7ed61aea1a8d6a19af2a5922a3c49666
Pinkie Wen <pinkiebala@gmail.com>
2023-08-07 10:33:42 +0800

    fix: hmr

--- background.js
+++ background.js
@@ -1,1 +1,2 @@
-import '@cooby/crx-load-script-webpack-plugin/lib/loadScript'
+import '@cooby/crx-load-script-webpack-plugin/lib/loadScript';
+console.log('background.js');
--- content.js
+++ content.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import {App}from './app'
 __webpack_public_path__ = chrome.runtime.getURL('')
 
 console.log(__webpack_public_path__)
 
-const print = () => {
-  console.log('as')
-}
-
-print()
-
-if (module.hot) {
-  module.hot.accept()
-}
-
-const App = () => {
-  return <div>TE1fST</div>
-}
 
 const div = document.createElement('div')
 document.body.append(div)
